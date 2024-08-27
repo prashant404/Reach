8b8635b88ac7b49edaa8f1f283b54a4e7bd5972f
Prashant Kumar <60027620+prashant404@users.noreply.github.com>
2024-08-27 12:34:28 +0530

    new

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,11 @@
+function AppBar() {
+  return (
+    <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+      <div className="">
+        ReachInbox
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,11 @@
+
+
+function TopBar() {
+  return (
+    <div className="h-16 w-screen bg-white  dark:bg-[#1F1F1F] fixed px-14 text-[#5B5F66] dark:text-white top-0 flex justify-between items-center border-b-2 dark:border-[#343A40] border-[#E0E0E0]">
+      <div>Onebox</div>
+    </div>
+  );
+}
+
+export default TopBar;
